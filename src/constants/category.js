d11fb277629a193babf4e3a82720dea867a521d4
Gods-Blessing <mohitadhana2569@gmail.com>
2023-06-09 11:58:46 +0530

    added hosted url

--- src/constants/category.js
+++ src/constants/category.js
@@ -1,37 +1,39 @@
+import image222 from '../assets/images/images222.jpg';
+
 export const arr = [
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     },
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     },
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     },
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     },
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     },
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     },
     {
         id:1,
-        image:'https://startupzworld.s3.ap-south-1.amazonaws.com/assets/images/category/6g106td0iLOqU4sCfo7d37TTmxUKFkJR.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY76JE5Z7H46UVM4Z%2F20230608%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20230608T062133Z&X-Amz-Expires=900&X-Amz-Signature=dde592c75dd07f499957e8143e52557e1eaae11ab7c885f2c21d10cc2179a4f9&X-Amz-SignedHeaders=host',
+        image:image222,
         category: 'Artificial Intelligence'
     }
 ];
